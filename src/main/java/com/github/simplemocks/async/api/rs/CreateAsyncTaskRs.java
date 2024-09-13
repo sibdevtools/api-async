017cde8701e260b10934698814f0a05f41b3f5c3
sibmaks <sibmaks@vk.com>
2024-09-13 21:27:16 +0300

    Unify API

--- src/main/java/com/github/simplemocks/async/api/rs/CreateAsyncTaskRs.java
+++ src/main/java/com/github/simplemocks/async/api/rs/CreateAsyncTaskRs.java
@@ -1,6 +1,8 @@
 package com.github.simplemocks.async.api.rs;
 
+import com.github.simplemocks.common.api.dto.ErrorRsDto;
 import com.github.simplemocks.common.api.rs.StandardBodyRs;
+import jakarta.annotation.Nonnull;
 
 /**
  * Create async task response
@@ -17,5 +19,14 @@ public class CreateAsyncTaskRs extends StandardBodyRs<Boolean> {
      */
     public CreateAsyncTaskRs(boolean status) {
         super(status);
+    }
+
+    /**
+     * Construct async task creation response with error
+     *
+     * @param error happened error
+     */
+    public CreateAsyncTaskRs(@Nonnull ErrorRsDto error) {
+        super(error);
     }
 }
